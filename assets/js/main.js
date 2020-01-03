e95eb6bf30342742223002abf2984dd17d4040e5
Borys <borys.olejniczak@antarisconsulting.com>
2020-01-03 09:24:23 +0000

    work with animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,8 +13,8 @@
     throttleDelay: 99,
 
     // offset: 120, // offset (in px) from the original trigger point
-  delay: 50, // values from 0 to 3000, with step 50ms
-  duration: 1000, // values from 0 to 3000, with step 50ms
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 650, // values from 0 to 3000, with step 50ms
   easing: 'ease', // default easing for AOS animations
   once: false, // whether animation should happen only once - while scrolling down
   mirror: false, // whether elements should animate out while scrolling past them
