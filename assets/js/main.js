2b15b2ef8449615bff826f72382a4e8784dc8e02
Borys <borys.olejniczak@antarisconsulting.com>
2020-01-03 12:04:44 +0000

    work with animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,20 +5,6 @@
 */
 
 (function($) {
-  // Launch animation lib
-  AOS.init({
-    debounceDelay: 50,
-    throttleDelay: 99,
-
-    // offset: 120, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 650, // values from 0 to 3000, with step 50ms
-    easing: "ease", // default easing for AOS animations
-    once: true, // whether animation should happen only once - while scrolling down
-    mirror: false, // whether elements should animate out while scrolling past them
-    anchorPlacement: "top-bottom" // defines which position of the element regarding to window should trigger the animation
-  });
-
   var $window = $(window),
     $body = $("body"),
     $wrapper = $("#page-wrapper"),
@@ -96,6 +82,20 @@
 })(jQuery);
 
 (function() {
+  // Launch animation lib
+  AOS.init({
+    debounceDelay: 50,
+    throttleDelay: 99,
+
+    // offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 650, // values from 0 to 3000, with step 50ms
+    easing: "ease", // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: "top-bottom" // defines which position of the element regarding to window should trigger the animation
+  });
+
   // Glider Config
   new Glider(document.querySelector(".glider"), {
     slidesToShow: 1.5,
@@ -132,3 +132,15 @@
     ]
   });
 })();
+AOS.init({
+  debounceDelay: 50,
+  throttleDelay: 99,
+
+  // offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 650, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: true, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom" // defines which position of the element regarding to window should trigger the animation
+});
