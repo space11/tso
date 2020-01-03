4b4766543b91b4e244c7c29f54bedf49545ecb0f
Borys <borys.olejniczak@antarisconsulting.com>
2020-01-03 09:06:55 +0000

    clean up

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,24 @@
 */
 
 (function($) {
+  
+  // Launch animation lib 
+  AOS.init({
+    startEvent: 'DOMContentLoaded',
+    debounceDelay: 50,
+    throttleDelay: 99,
+
+    offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+
+  });
+
   var $window = $(window),
     $body = $("body"),
     $wrapper = $("#page-wrapper"),
